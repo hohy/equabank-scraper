896bff151b5e45c4963a3d0897a1c66451e0b81e
Jan Hybl <jan.hybl@strv.com>
2016-10-30 15:03:16 +0100

    Code refactored to modules without classes. Modules are better for this use case.

--- index.js
+++ index.js
@@ -7,37 +7,34 @@ const cheerio = require('cheerio')
 /**
  * Main module of the Equabank-scraper library.
  */
-class EquabankScraper {
 
-  constructor() {
-    // create http client instance
-    this.http = axios.create({
-      baseURL: 'https://www.equabanking.cz/IBS/',
-      timeout: 5000,
-      headers: {
-        Accept: 'text/html,application/xhtml+xml,application/xml',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
+// http client instance
+const http = axios.create({
+  baseURL: 'https://www.equabanking.cz/IBS/',
+  timeout: 5000,
+  headers: {
+    Accept: 'text/html,application/xhtml+xml,application/xml',
+    'Content-Type': 'application/x-www-form-urlencoded'
   }
+})
 
-  /**
-   * Function loads the login page of the internet banking, eneters login
-   * credentials and send a request to the server.
-   * @param {String} username Username for Equabanking
-   * @param {String} password Password for Equabanking
-   * @param {function} callback Callback function executed when login is finished
-   * @returns {Promise} Object with basic info about accounts from the home page
-   */
-  login(username, password, callback) {
-    console.log('Loading login page...')
+/**
+ * Function loads the login page of the internet banking, eneters login
+ * credentials and send a request to the server.
+ * @param {String} username Username for Equabanking
+ * @param {String} password Password for Equabanking
+ * @param {function} callback Callback function executed when login is finished
+ * @returns {Promise} Object with basic info about accounts from the home page
+ */
+function login(username, password, callback) {
+  console.log('Loading login page...')
     // load the login page
-    const getLoginData = {
-      javaVM: 7,
-      command: 'auth_loginByPasswordPage',
-      staticCmd: 'null'
-    }
-    return this.http.post('/ControllerServlet', utils.object2FormData(getLoginData))
+  const getLoginData = {
+    javaVM: 7,
+    command: 'auth_loginByPasswordPage',
+    staticCmd: 'null'
+  }
+  return http.post('/ControllerServlet', utils.object2FormData(getLoginData))
     .then(response => {
       console.log('Login page loaded.')
       if (response.status === 200) {
@@ -77,7 +74,7 @@ class EquabankScraper {
         loginByMK_password: password
       }
       console.log('Performing login, loading home page ...')
-      return this.http.post(`/${loginData.loginAction}`, utils.object2FormData(loginFormData))
+      return http.post(`/${loginData.loginAction}`, utils.object2FormData(loginFormData))
       .then(response => {
         if (response.status === 200) {
           return response.data
@@ -89,28 +86,25 @@ class EquabankScraper {
       return utils.writeToFile('/tmp/home.html', homePage)
     })
     // process the main page
-    .then(this.parseHomePage)
+    .then(parseHomePage)
     .then(callback)
-  }
-
-  parseHomePage(homePage) {
-    const $ = cheerio.load(homePage)
-    if ($('body#homepage')) {
-      // get list of accounts
-      const accounts = []
-      const accountElements = $('table#currentAccountsTableId tr').next()
-      accountElements.each((index, account) => {
-        account = $(account)
-        const id = account.find('td.account span').text()
-        const name = account.find('td.account a').text()
-        const balance = account.find('td.total').text()
-        accounts.push({ id, name, balance })
-      })
-      return accounts
-    }
-    throw new Error('Error while paring home page')
-  }
-
 }
+module.exports.login = login
 
-module.exports = EquabankScraper
+function parseHomePage(homePage) {
+  const $ = cheerio.load(homePage)
+  if ($('body#homepage')) {
+      // get list of accounts
+    const accounts = []
+    const accountElements = $('table#currentAccountsTableId tr').next()
+    accountElements.each((index, account) => {
+      account = $(account)
+      const id = account.find('td.account span').text()
+      const name = account.find('td.account a').text()
+      const balance = account.find('td.total').text()
+      accounts.push({ id, name, balance })
+    })
+    return accounts
+  }
+  throw new Error('Error while paring home page')
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,13 @@
 'use strict'
 
 const expect = require('chai').expect
-const EquabankScraper = require('../index')
+const equabank = require('../index')
 const fs = require('fs')
 
 describe('Lets do some basic testing', () => {
-
-  it('Should create a new instance of the scraper', () => {
-    const equabank = new EquabankScraper()
-    expect(equabank).not.to.be.equal(null)
-  })
-
   it('Should log in and return accounts data',
     function test() {
       this.timeout(10000)
-      const equabank = new EquabankScraper()
       return equabank.login(process.env.EQUA_USERNAME, process.env.EQUA_PASSWORD)
         .then(results => {
           console.log(JSON.stringify(results))
@@ -23,12 +16,10 @@ describe('Lets do some basic testing', () => {
         })
     })
 
-    it('Should parse the home page', (done) => {
-      const equabank = new EquabankScraper()
-      const homePage = fs.readFileSync('./resources/home.html')
-      const result = equabank.parseHomePage(homePage)
-      console.log(JSON.stringify(result))
-      done()
-    })
-
+  it('Should parse the home page', done => {
+    const homePage = fs.readFileSync('./resources/home.html')
+    const result = equabank.parseHomePage(homePage)
+    console.log(JSON.stringify(result))
+    done()
+  })
 })
